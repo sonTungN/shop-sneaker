9855c8509f7223a9c75151bd7cf288164cd261dd
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-08-10 21:32:30 +0700

    add: create `User` model

--- src/app/model/User.js
+++ src/app/model/User.js
@@ -2,4 +2,14 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const User = new Schema({});
+const User = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", User);
